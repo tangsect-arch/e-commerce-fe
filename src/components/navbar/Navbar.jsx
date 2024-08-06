0801582a29b41466762afbc7887cf77066f5a8c7
Sabari <sabari288@gmail.com>
2024-08-06 23:19:00 +0530

    navbar

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+import "./Navbar.css";
+import cart_icon from "../assets/cart_icon.png";
+import logo from "../assets/logo.png";
+
+export const Navbar = () => {
+  const [menu, setMenu] = useState("shop");
+  console.log(menu);
+  return (
+    <>
+      <div className="navbar">
+        <div className="nav-logo">
+          <img src={logo} alt="" />
+          <p>SHOPPER</p>
+        </div>
+        <ul className="nav-menu">
+          <li
+            onClick={() => {
+              setMenu("shop");
+            }}
+          >
+            Shop
+            {menu === "shop" && <hr />}
+          </li>
+          <li
+            onClick={() => {
+              setMenu("men");
+            }}
+          >
+            Men{menu === "men" && <hr />}
+          </li>
+          <li
+            onClick={() => {
+              setMenu("women");
+            }}
+          >
+            Women{menu === "women" && <hr />}
+          </li>
+          <li
+            onClick={() => {
+              setMenu("kids");
+            }}
+          >
+            Kids{menu === "kids" && <hr />}
+          </li>
+        </ul>
+        <div className="nav-login-cart">
+          <button>Login</button>
+          <img src={cart_icon} alt="" />
+          <div className="nav-cart-count">0</div>
+        </div>
+      </div>
+    </>
+  );
+};
