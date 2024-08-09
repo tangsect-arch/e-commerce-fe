18473bf24bb414561167451bd71edb28a53cf6f9
sabari.pn <c-sPraharshan@ihyphen.com>
2024-08-09 16:12:20 +0530

    homepage

--- src/components/shop/ShopComponent.jsx
+++ src/components/shop/ShopComponent.jsx
@@ -9,17 +9,19 @@ export default function ShopComponent() {
     // <div className="shop-container">
     <div className="shop-component">
       <div className="left-component">
-        <h2>NEW ARRAIVALS ONLY</h2>
-        <h1>
-          new <img className="img-class" src={hand_icon} alt="" />
-          <br />
-          collections <br />
-          for everyone
-        </h1>
-        <button>
-          Latest Collection
-          <img className="arrow" src={arrow} alt="" />
-        </button>
+        <div className="left-container">
+          <h2>NEW ARRAIVALS ONLY</h2>
+          <h1>
+            new <img className="img-class" src={hand_icon} alt="" />
+            <br />
+            collections <br />
+            for everyone
+          </h1>
+          <button>
+            Latest Collection
+            <img className="arrow" src={arrow} alt="" />
+          </button>
+        </div>
       </div>
       <div className="right-component">
         <img className="right-image" src={background} alt="" />
