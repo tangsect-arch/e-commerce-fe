c18f3691b468ccfc67020f44dde422e4f360287e
sabari.pn <c-sPraharshan@ihyphen.com>
2024-08-10 08:26:56 +0530

    footer

--- src/components/home/PopularItems.jsx
+++ src/components/home/PopularItems.jsx
@@ -6,6 +6,9 @@ import items from "../assets/data.js";
 export const PopularItems = () => {
   return (
     <>
+      <div className="popular-text">
+        <h2>POPULAR IN WOMEN</h2>
+      </div>
       <Items items={items} />
     </>
   );
